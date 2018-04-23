0fa1a6c6ae3bca768a7e77822cda293ded0d572b
Jonathan Worent <jonathan@inspiredbytes.net>
2018-04-23 08:06:31 -0500

    Allow CORS

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const events = require('./data/events');
 
 const server = express();
 
-server.use(express.static(`${__dirname}/public`))
+server.use(express.static(`${__dirname}/public`));
+
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 server.get('/events', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
