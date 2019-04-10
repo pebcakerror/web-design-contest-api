b3f93291da325da1246a1ba780c9ca85f1b8041f
Jonathan Worent <jworent@burwood.com>
2019-04-10 11:45:00 -0500

    events data for 2019 Illinois State

--- data/events.js
+++ data/events.js
@@ -1,74 +1,50 @@
 module.exports = [
   {
-    name: "Christy's Place",
+    name: "I Know You Like A Book",
     address: {
-      street:  "1221 Peoria St",
-      city: "Washington",
+      street:  "4707 N Prospect Rd",
+      city: "Peoria Heights",
       state: "Illinois",
       state_abbr: "IL",
-      zip: "61571"
+      zip: "61616"
     },
-    date: "Jun. 29, 2018",
+    date: "Aug. 3, 2018",
     time: "7:00 PM"
   },
   {
-    name: "Red Barn",
+    name: "The Book Nook",
     address: {
-      street:  "621 W Glen Ave",
+      street:  "6944 N University St",
+      city: "Peoria",
+      state: "Illinois",
+      state_abbr: "IL",
+      zip: "61615"
+    },
+    date: "Sep. 7, 2018",
+    time: "7:00 PM"
+  },
+  {
+    name: "Lit. on Fire Used Books",
+    address: {
+      street:  "712 W Main St",
+      city: "Peoria",
+      state: "Illinois",
+      state_abbr: "IL",
+      zip: "61606"
+    },
+    date: "Oct. 5, 2018",
+    time: "6:30 PM"
+  },
+  {
+    name: "Book Rack Used Books",
+    address: {
+      street:  "408 N Knoxville Ave #2B",
       city: "Peoria",
       state: "Illinois",
       state_abbr: "IL",
       zip: "61614"
     },
-    date: "Jul. 11, 2018",
-    time: "9:00 PM"
-  },
-  {
-    name: "Rhythm Kitchen Music Cafe",
-    address: {
-      street:  "305 SW Water St",
-      city: "Peoria",
-      state: "Illinois",
-      state_abbr: "IL",
-      zip: "61602"
-    },
-    date: "Jul. 24, 2018",
-    time: "7:00 PM"
-  },
-  {
-    name: "Six Strings",
-    address: {
-      street:  "525 N Center St",
-      city: "Bloomington",
-      state: "Illinois",
-      state_abbr: "IL",
-      zip: "61701"
-    },
-    date: "Aug. 10, 2018",
-    time: "8:00 PM"
-  },
-  {
-    name: "Cadillac Jack's Cantina & Grill",
-    address: {
-      street:  "1507 S Main St",
-      city: "Bloomington",
-      state: "Illinois",
-      state_abbr: "IL",
-      zip: "61701"
-    },
-    date: "Aug. 22, 2018",
-    time: "8:30 PM"
-  },
-  {
-    name: "Contemporary Art Center",
-    address: {
-      street:  "305 SW Water St",
-      city: "Peoria",
-      state: "Illinois",
-      state_abbr: "IL",
-      zip: "61602"
-    },
-    date: "Sept. 3, 2018",
-    time: "5:00 PM"
+    date: "Nov. 5, 2018",
+    time: "7:15 PM"
   }
 ]
